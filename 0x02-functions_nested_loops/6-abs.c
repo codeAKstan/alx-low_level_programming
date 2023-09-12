00020a58c9d1f7a5b6c80b6649d33d78cb023bcf
codeAKstan <boystanismoses@gmail.com>
2023-09-12 13:08:29 -0700

    done with mandatory

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs - entry point
+ *int - recives a parameter of type int
+ *@n: the int to check
+ *Return: an absolute type number
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+		return (-n);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *print_last_digit - prints the last digit of a number
+ *@n: the no to check
+ *Return: the last digit
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
