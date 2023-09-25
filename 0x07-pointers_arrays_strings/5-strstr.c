fb6b148c74f8c9e432c6ddc403c499bd29355a12
codeAKstan <boystanismoses@gmail.com>
2023-09-25 10:34:49 -0700

    Added task 3 to 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strstr - entry point
+ * @haystack: the main string
+ * @needle: the substring
+ * Return: the return type char
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (!needle[j])
+			return (&haystack[i]);
+	}
+	return (NULL);
+}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ * print_chessboard - entry point
+ * @a: first part of the 2 dimensional array
+ * Return: void
+ */
