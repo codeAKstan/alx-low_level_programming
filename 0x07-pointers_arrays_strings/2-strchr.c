603e73b3aa66e520d4ecce0b797f543327470feb
codeAKstan <boystanismoses@gmail.com>
2023-09-25 09:50:26 -0700

    Added task 2, strcpy

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr -  entry point
+ * @s: the main string
+ * @c: the substring to find
+ * Return: the return type is a char or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&(s[i]));
+		}
+	}
+	return (NULL);
+}
