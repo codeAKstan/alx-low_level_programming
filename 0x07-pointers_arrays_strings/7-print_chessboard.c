aca0d92e95f11d149e87e863359a3f1789f44067
codeAKstan <boystanismoses@gmail.com>
2023-09-25 14:07:46 -0700

    Added task 5 and 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,3 +5,17 @@
  * @a: first part of the 2 dimensional array
  * Return: void
  */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
