65a0631458483659bbb6152d4166891d954dbeab
codeAKstan <boystanismoses@gmail.com>
2023-10-04 11:18:25 -0700

    checked for failure and added the right amount of memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,39 +2,46 @@
 #include <stdlib.h>
 #include <limits.h>
 
-/**
- * str_concat - the entry point - function to concat two strings
- * @s1: the destination string
- * @s2: the second string
- * Return: NULL on failure
+/**                                                                                               
+ * str_concat - the entry point - function to concat two strings                                  
+ * @s1: the destination string                                                                    
+ * @s2: the second string                                                                         
+ * Return: NULL on failure                                                                        
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *str;
-	int len = 0;
-	int i, j;
+        char *str;
+        int len = 0;
+	int slen = 0;
+        int i, j;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+        if (s1 == NULL || s2 == NULL)
+                return (NULL);
 
-	while (s1[len] != '\0')
-	{
-		len++;
-	}
+        while (s1[len] != '\0')
+        {
+                len++;
+        }
 
-	str = (char *)malloc((len + 1) * sizeof(char));
+	while (s2[slen] != '\0')
+		slen++;
 
-	for (i = 0; i < len; i++)
-	{
-		str[i] = s1[i];
-	}
+        str = (char *)malloc((len + slen + 1) * sizeof(char));
 
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		str[i + j] = s2[j];
-	}
+	if (str == NULL)
+		return NULL;
 
-	str[i + j] = '\0';
-	return (str);
+        for (i = 0; i < len; i++)
+        {
+                str[i] = s1[i];
+        }
+
+        for (j = 0; s2[j] != '\0'; j++)
+        {
+                str[i + j] = s2[j];
+        }
+
+        str[i + j] = '\0';
+        return (str);
 }
