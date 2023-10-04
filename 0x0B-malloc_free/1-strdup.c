d1cec108a4bc2931eb754351f058c5e7c92d50af
codeAKstan <boystanismoses@gmail.com>
2023-10-04 06:59:17 -0700

    Added task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * _strdup - entry point - returns a pointer to space memory
+ * @str: the string to return
+ * Return: return NULL if str is NULL
+ * On succes return pointer to the duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	unsigned int i;
+	unsigned int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	s = (char *)malloc(len * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		s[i] = str[i];
+
+	return (s);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,2 +1,3 @@
 0x0B-malloc_free
 task 0 - a func to create an array
+task 1 - a func that returns a newly allocated memory, which contains a copy of the string
