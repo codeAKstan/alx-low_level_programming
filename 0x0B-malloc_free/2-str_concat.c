3ac40b33fee7c06925be84573e6248841a81a275
codeAKstan <boystanismoses@gmail.com>
2023-10-04 11:34:59 -0700

    Betty style and empty string return value corrected

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,47 +1,50 @@
 #include "main.h"
 #include <stdlib.h>
-#include <limits.h>
 
-/**                                                                                               
- * str_concat - the entry point - function to concat two strings                                  
- * @s1: the destination string                                                                    
- * @s2: the second string                                                                         
- * Return: NULL on failure                                                                        
+/**
+ * str_concat - function to concatenate two strings
+ * @s1: the destination sting
+ * @s2: the second string
+ * Return: NULL if the allocation failed
  */
-
 char *str_concat(char *s1, char *s2)
 {
-        char *str;
-        int len = 0;
-	int slen = 0;
-        int i, j;
+	char *str;
+	size_t len1 = 0;
+	size_t len2 = 0;
+	size_t i, j;
 
-        if (s1 == NULL || s2 == NULL)
-                return (NULL);
+	if (s1 != NULL)
+	{
+		while (s1[len1] != '\0')
+		{
+			len1++;
+		}
+	}
 
-        while (s1[len] != '\0')
-        {
-                len++;
-        }
+	if (s2 != NULL)
+	{
+		while (s2[len2] != '\0')
+		{
+			len2++;
+		}
+	}
 
-	while (s2[slen] != '\0')
-		slen++;
-
-        str = (char *)malloc((len + slen + 1) * sizeof(char));
+	str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 
-        for (i = 0; i < len; i++)
-        {
-                str[i] = s1[i];
-        }
+	for (i = 0; i < len1; i++)
+	{
+		str[i] = s1[i];
+	}
 
-        for (j = 0; s2[j] != '\0'; j++)
-        {
-                str[i + j] = s2[j];
-        }
+	for (j = 0; j < len2; j++)
+	{
+		str[i + j] = s2[j];
+	}
 
-        str[i + j] = '\0';
-        return (str);
+	str[i + j] = '\0';
+	return (str);
 }
