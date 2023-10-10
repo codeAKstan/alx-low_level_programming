9cb38cdcae4c24677e854dbd4d924e8776f6e154
codeAKstan <boystanismoses@gmail.com>
2023-10-10 13:17:42 -0700

    Added task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_void - Entry point
+ * @d: a pointer of type dog_t
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
